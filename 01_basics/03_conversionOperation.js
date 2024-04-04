083b9fa8c6406ba483e0a4551f977fada104a5e0
RayeesaBanu <150254678+RayeesaBanu@users.noreply.github.com>
2024-04-04 08:14:31 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,63 @@
+// let score="35abc"
+
+// console.log(typeof score)
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let score=null
+
+// console.log(typeof score)
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let score=undefined
+
+// console.log(typeof score)
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let score=true
+// console.log(typeof score)
+// console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+//"33"=>33
+//"35abc"=>NaN
+//true=>1; false=>0
+
+
+// let isLoggedIn=1
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// let isLoggedIn=""
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+//1=>ture;  0=>false
+//""=>false
+//"Raeesa"=>true
+
+
+let someNumer=33
+let stringNumber=String(someNumer)
+console.log(stringNumber);
+console.log(typeof stringNumber);
